eda20195e212a8abeec6627cecaf7feb5bb67326
richmore <richmore@gmail.com>
2023-02-21 10:27:43 -0800

    codec 0x02

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
- * main - check the code
- *
- * Return: Always 0
+ * print_sign - Prints the sign of a number
+ * @n: The number to be checked
+ * Return: 1 for positive num, -1 for negative num for anything
  */
 
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
