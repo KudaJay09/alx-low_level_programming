a4f4d6431334e4cc3cf35dcb75a84e986ea458a7
richmore <richmore@gmail.com>
2023-02-23 05:12:10 -0800

    0x04

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,20 +4,20 @@
  * print_most_numbers - prints the numbers, from 0 to 9
  * Return: Always 0
  */
-void print_moost_numbers(void)
+void print_most_numbers(void)
 {
-	int c;
+	int x = 0;
 
-	for (c = 48; c < 58; c++)
+	for (; x <= 9; x++)
 	{
-		if (c != 50)
+		if (x == 2 || x == 4)
 		{
-			if (c != 52)
-			{
-				_putchar(c);
-			}
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
 		}
 	}
-
 	_putchar('\n');
 }
